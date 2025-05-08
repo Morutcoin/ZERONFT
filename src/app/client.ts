4d38cdee53d1b018157cfe4bc75f2ff541ae0076
forgetzz <forget.noxa90@gmail.com>
2025-05-08 23:57:03 +0800

    n

--- src/app/client.ts
+++ src/app/client.ts
@@ -1,6 +1,6 @@
 import { createThirdwebClient } from "thirdweb";
-const api = "15adf8e9820f3d79ca3602f05f0425d5"
-const clientId = api;
+
+const clientId = process.env.NEXT_PUBLIC_CLIENT_ID;
 
 if (!clientId) {
   throw new Error("Missing NEXT_PUBLIC_CLIENT_ID environment variable");
