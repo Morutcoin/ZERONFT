2f3add929a68ee7c7f9b9acc0c6dfe234184cf14
forgetzz <forget.noxa90@gmail.com>
2025-05-08 22:38:16 +0800

    n

--- utils/contracts.ts
+++ utils/contracts.ts
@@ -3,9 +3,9 @@ import { client } from "@/app/client";
 import { getContract } from "thirdweb";
 import { stakingABI } from "./stakingABI";
 
-const nftContractAddress = "<contract_address>";
-const rewardTokenContractAddress = "<contract_address>";
-const stakingContractAddress = "<contract_address>";
+const nftContractAddress = "0xa519F95A269825D49Dc7eb9e5297C88c4b6B85b0";
+const rewardTokenContractAddress = "0x504D546E7637d33D9508b5bd147aDB5F05c3E3Ba";
+const stakingContractAddress = "0xD9eb5d0FD5cD3e656949D5140B48cB1BE96686dA";
 
 export const NFT_CONTRACT = getContract({
     client: client,
